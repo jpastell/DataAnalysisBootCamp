b9eb3c2f1ccc723f51aab5e3fc78b053ac9d39db
jpastell <castellanos.juan.pablo@gmail.com>
2019-08-24 19:17:14 -0500

    update image

--- java_script/static/js/app.js
+++ java_script/static/js/app.js
@@ -24,19 +24,14 @@ function tableInit(my_data) {
 var submit = d3.select("#filter-btn");
 
 submit.on("click", function() {
-
   // Prevent the page from refreshing
   d3.event.preventDefault();
-
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
-
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
-
   console.log(inputValue);
   //console.log(people);
-
   var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
   console.log(filteredData);
   tableInit(filteredData);
