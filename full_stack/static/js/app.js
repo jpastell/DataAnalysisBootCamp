03a728e926b48de1d69aa47043e415b7139df8f9
jpastell <castellanos.juan.pablo@gmail.com>
2019-09-04 22:10:51 -0500

    Apdating plots

--- full_stack/static/js/app.js
+++ full_stack/static/js/app.js
@@ -15,31 +15,36 @@ function buildMetadata(sample) {
         //selector.append("p").attr('class', 'card-text').append("small").text(key+":\n"+meta[key]);
         selector.append("p").attr('class', 'card-text').text(key+":\n"+meta[key]);
     }
+    // BONUS: Build the Gauge Chart
+    buildGauge(meta.WFREQ);
   });
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
 }
 
 function buildCharts(sample) {
-
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-
-  // @TODO: Build a Bubble Chart using the sample data
-
-  // @TODO: Build a Pie Chart
-  // HINT: You will need to use slice() to grab the top 10 sample_values,
-  // otu_ids, and labels (10 each).
-
-  //Clear the previos plot
-  Plotly.purge("pie");
   dataURL = '/samples/'
   var sampleURL = dataURL.concat(sample);
   d3.json(sampleURL).then((data) => {
-      //var layout = { margin: { t: 0 } }
+      //[-- Build a Pie Buble --]
+      Plotly.purge("bubble");
+      var bubleTrace = {
+        x:data.otu_ids,
+        y : data.sample_values,
+        mode: 'markers',
+        hoverinfo:"text",
+        hovertext:data.otu_labels,
+        marker: {
+          size: data.sample_values,
+          color: data.otu_ids
+        }
+      };
+      Plotly.plot("bubble", [bubleTrace]);
+      //[-- Build a Pie Chart --]
       outIds = data.otu_ids.slice(0,10);
       sampVal = data.sample_values.slice(0,10);
       outLabels = data.otu_labels.slice(0,10);
+      //Clear the previos plot
+      Plotly.purge("pie");
       var pieTrace = {
         values : sampVal,
         labels:outIds,
@@ -47,8 +52,6 @@ function buildCharts(sample) {
         hovertext:outLabels,
         type: 'pie'
       };
-
-      //console.log(slicedData);
       Plotly.plot("pie", [pieTrace]);
   });
 }
--- full_stack/static/js/bonus.js
+++ full_stack/static/js/bonus.js
@@ -1,1 +1,104 @@
+function buildGauge(freq)
+{
 
+	//Erase the previos table
+	Plotly.purge("gauge");
+	var level = freq;
+
+	//Avoid nukks for the math operations
+	if ( level == null )
+	{
+		level = 0;
+	} else if ( level > 9 ) {
+		level = 9;
+	}
+
+	//The data look skewd in th egraph afte rthe degree calulation; Therfore, I
+	//defined levels for each pie partition and calculate fractions base don teh diference
+	//between each section
+	var truncated = Math.trunc(level);
+	var decimals = level %1 ;
+	//Value per index
+	//								  0    1    2	 3   4   5   6   7   8  9
+	var needle_base = new Array(	180, 155, 132, 113, 97, 82, 68, 49, 25, 0);
+	var needle_diff = new Array(	needle_base[0]-needle_base[1],
+									needle_base[1]-needle_base[2],
+									needle_base[2]-needle_base[3],
+									needle_base[3]-needle_base[4],
+									needle_base[4]-needle_base[5],
+									needle_base[5]-needle_base[6],
+									needle_base[6]-needle_base[7],
+									needle_base[7]-needle_base[8],
+									needle_base[8]-needle_base[9],
+									0);
+	//Degrees calculation
+	var degrees = needle_base[truncated] - (decimals * needle_diff[truncated]);
+	radius = .5;
+	var radians = degrees * Math.PI / 180;
+	var x = radius * Math.cos(radians);
+	var y = radius * Math.sin(radians);
+
+
+	// Path parameter creation
+	var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
+	     pathX = String(x),
+	     space = ' ',
+	     pathY = String(y),
+	     pathEnd = ' Z';
+
+   	//Path definition for the neddle 
+	var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+	//Data drawing
+	var data = [{ type: 'category',
+	   x: [0], y:[0],
+	    marker: {size: 28, color:'850000'},
+	    showlegend: false,
+	    name: 'speed',
+	    text: level,
+	    hoverinfo: 'text+name'},
+	  { values: [20,20,20,20,20,20,20,20,20, 180],
+	  rotation: 90,
+	  text: ['0-1', '1-2', '2-3', '3-4','4-5', '5-6', '6-7', '7-8', '8-9',''],
+	  textinfo: 'text',
+	  textposition:'inside',      
+	  marker: {colors:[	'rgba(169, 223, 191, .9)',
+	  					'rgba(162, 217, 206, .9)',
+	  					'rgba(163, 228, 215, .9)',
+	  					'rgba(174, 214, 241, .9)',
+	  					'rgba(169, 204, 227, .9)',
+	  					'rgba(210, 180, 222, .9)',
+	  					'rgba(215, 189, 226, .9)',
+						'rgba(245, 183, 177, .9)',
+	  					'rgba(230, 176, 170, .9)',
+	                    'rgba(255, 255, 255, 0)']},
+	  labels: ['0-1', '1-2', '2-3', '3-4','4-5', '5-6', '6-7', '7-8', '8-9'],
+	  hoverinfo: 'label',
+	  direction: "clockwise",
+	  hole: .5,
+	  type: 'pie',
+	  showlegend: false
+	}];
+
+	//Layout definition
+	var layout = {
+	  shapes:[{
+	      type: 'path',
+	      path: path,
+	      fillcolor: '850000',
+	      line: {
+	        color: '850000'
+	      }
+	    }],
+		title: 'Belly Button Washing Frequncy',
+		height: 500,
+		width: 600,
+		xaxis: {type:'category',zeroline:false, showticklabels:false,
+		            showgrid: false, range: [-1, 1]},
+		yaxis: {type:'category',zeroline:false, showticklabels:false,
+		            showgrid: false, range: [-1, 1]}
+	};
+
+	//Draw the plot
+	Plotly.plot("gauge",data,layout);
+}
